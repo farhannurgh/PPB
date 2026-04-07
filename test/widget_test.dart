8893585d79a01c562a38de5cb5bb8d88bb438447
farhannurgh <mfarhan.nugroho004@gmail.com>
2026-04-08 02:20:10 +0700

    CRUD

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:crud_ppb/main.dart';
+
+void main() {
+  test('MyApp widget can be created', () {
+    expect(const MyApp(), isNotNull);
+  });
+}
