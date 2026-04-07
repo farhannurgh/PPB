9debc08a185f245bc7b57b4c05b170d80de3d159
farhannurgh <163228281+farhannurgh@users.noreply.github.com>
2026-04-08 02:36:57 +0700

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:crud_ppb/main.dart';
-
-void main() {
-  test('MyApp widget can be created', () {
-    expect(const MyApp(), isNotNull);
-  });
-}
